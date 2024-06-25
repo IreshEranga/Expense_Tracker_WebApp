3f2de4f55af5a8ebcbf7cbd4a4c025dd81e8ce1d
Iresh Eranga <iresheranga2020al@gmail.com>
2024-06-25 10:34:23 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import NavBar from '../../components/Navbar'
+import Graph from '../../components/Graph'
 
 
 const Home = () => {
   return (
     <div>
-        <NavBar/>
-        <h1>Home</h1>
+        <NavBar />
+        <h1 
+          className='text-4xl py-8 mb-10 text-white rounded' 
+          style={{ backgroundColor: 'slategray', textAlign:'center', padding:'20px 0' }}
+        >
+          Expense Tracker
+        </h1>
+
+        <div className="grid md:grid-cols-2 gap-4">
+          {/* chart */}
+          <Graph/>
+
+          {/* form */}
+        </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
