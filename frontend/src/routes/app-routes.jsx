63ec4a58e3f1b607c20c8cc246a45ab869a914cf
Iresh Eranga <iresheranga2020al@gmail.com>
2024-06-25 08:02:53 +0530

    Update app-routes.jsx
    Initially goes to the login page

--- frontend/src/routes/app-routes.jsx
+++ frontend/src/routes/app-routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import LogInPg from "../pages/LogIn/LogInPg";
 import Register from "../pages/Register/Register";
@@ -6,16 +6,17 @@ import Home from "../pages/Home/Home";
 
 const AppRoutes = () => {
     return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/login" element={<LogInPg/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/home" element={<Home/>}/>
-                </Routes>
-            </Router>
-        </>
-    )
+        <Router>
+            <Routes>
+                <Route path="/login" element={<LogInPg />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Navigate to="/login" />} />
+                {/* Add other routes here */}
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRoutes;
